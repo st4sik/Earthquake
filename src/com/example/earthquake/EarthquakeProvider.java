da7ad1ab616e8ad13ca0f1d747a1cb4766d2edcd
st4sik <ryabokonsm@mail.ru>
2014-05-17 14:32:45 +0400

    Создание провайдера

---
+++ src/com/example/earthquake/EarthquakeProvider.java
@@ -0,0 +1,206 @@
+package com.example.earthquake;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+public class EarthquakeProvider extends ContentProvider {
+
+	public static final Uri CONTENT_URI=Uri.parse("content://com.example.earthprovider/earthquakes");
+	
+	public static final String KEY_ID="_id";
+	public static final String KEY_DATE="date";
+	public static final String KEY_DETAILS="details";
+	public static final String KEY_SUMMARY="summary";
+	public static final String KEY_LOCATION_LAT="latitude";
+	public static final String KEY_LOCATION_LNG="longitude";
+	public static final String KEY_MAGNITUDE="magnitude";
+	public static final String KEY_LINK="link";
+	
+	private static class EarthquakeDatabaseHelper extends SQLiteOpenHelper
+	{
+
+		private static final String TAG="EarthquakeProvider";
+		public static final String DATABASE_NAME="earthquakes.db";
+		public static final int DATABASE_VERSION=1;
+		public static final String EARTHQUAKE_TABLE="earthquakes";
+		
+		public static final String DATABASE_CREATE=
+				"create table "+	EARTHQUAKE_TABLE+	" ("
+				+	KEY_ID	+ " integer primary key autoincrement, "
+				+	KEY_DATE	+	" INTEGER, "
+				+ 	KEY_DETAILS	+	" TEXT, "
+				+	KEY_SUMMARY	+	" TEXT, "
+				+	KEY_LOCATION_LAT	+	" FLOAT, "
+				+	KEY_LOCATION_LNG	+	" FLOAT, "
+				+	KEY_MAGNITUDE	+	" FLOAT, "
+				+	KEY_LINK	+	" TEXT);";
+		
+		private SQLiteDatabase earthquakeDB;
+		
+		
+		
+		public EarthquakeDatabaseHelper(Context context, String name,
+				CursorFactory factory, int version) {
+			super(context, name, factory, version);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(DATABASE_CREATE);
+			
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+			db.execSQL("DROP TABLE IF EXISTS "+EARTHQUAKE_TABLE);
+			onCreate(db);
+			
+			
+		}
+		
+	}
+	@Override
+	public int delete(Uri uri, String where, String[] whereArgs) {
+		SQLiteDatabase database = dbHelper.getWritableDatabase();
+	    
+	    int count;
+	    switch (uriMatcher.match(uri)) {
+	      case QUAKES:
+	        count = database.delete(
+	          EarthquakeDatabaseHelper.EARTHQUAKE_TABLE, where, whereArgs);
+	        break;
+	      case QUAKE_ID:
+	        String segment = uri.getPathSegments().get(1);
+	        count = database.delete(EarthquakeDatabaseHelper.EARTHQUAKE_TABLE, 
+	                KEY_ID + "=" 
+	                + segment
+	                + (!TextUtils.isEmpty(where) ? " AND ("
+	                + where + ')' : ""), whereArgs);
+	        break;
+
+	      default: throw new IllegalArgumentException("Unsupported URI: " + uri);
+	    }
+
+	    getContext().getContentResolver().notifyChange(uri, null);
+	    return count;
+
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		switch(uriMatcher.match(uri))
+		{
+		case QUAKES:return "vnd.android.cursor.dir/vnd.example.earthquake";
+		case QUAKE_ID: return "vnd.android.cursor.item/vnd.example.earthquake";
+		default: throw new IllegalArgumentException("Unsupported URI: "+uri);
+		
+		}
+		
+	}
+
+	@Override
+	public Uri insert(Uri _uri, ContentValues values) {
+		SQLiteDatabase database = dbHelper.getWritableDatabase();
+		long rowID = database.insert(
+	      EarthquakeDatabaseHelper.EARTHQUAKE_TABLE, "quake", values);
+		
+	    if (rowID > 0) {
+	      Uri uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+	      getContext().getContentResolver().notifyChange(uri, null);
+	      return uri;
+	    }
+	    
+	    throw new SQLException("Failed to insert row into " + _uri);
+	}
+	
+	EarthquakeDatabaseHelper dbHelper;
+	
+	@Override
+	public boolean onCreate() {
+		
+		Context context=getContext();
+		dbHelper=new EarthquakeDatabaseHelper(context, EarthquakeDatabaseHelper.DATABASE_NAME, null, EarthquakeDatabaseHelper.DATABASE_VERSION);
+		
+		return true;
+	}
+
+	private static final int QUAKES=1;
+	private static final int QUAKE_ID=2;
+	
+	private static final UriMatcher uriMatcher;
+	
+	static{
+		uriMatcher=new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI("com.example.earthprovider", "earthquakes", QUAKES);
+		uriMatcher.addURI("com.example.earthprovider", "earthquakes/#", QUAKE_ID);
+	}
+	
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		
+		SQLiteDatabase database = dbHelper.getWritableDatabase();
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		qb.setTables(EarthquakeDatabaseHelper.EARTHQUAKE_TABLE);
+
+		switch (uriMatcher.match(uri)) {
+		      case QUAKE_ID: qb.appendWhere(KEY_ID + "=" + uri.getPathSegments().get(1));
+		                     break;
+		      default      : break;
+		    }
+		String orderBy;
+		if (TextUtils.isEmpty(sortOrder)) {
+		      orderBy = KEY_DATE;
+		    } else {
+		      orderBy = sortOrder;
+		    }
+		Cursor c = qb.query(database,
+		                        projection,
+		                        selection, selectionArgs,
+		                        null, null,
+		                        orderBy);
+		c.setNotificationUri(getContext().getContentResolver(), uri);
+		return c;
+		}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String where,
+			String[] whereArgs) {
+		SQLiteDatabase database = dbHelper.getWritableDatabase();
+	    
+	    int count;
+	    switch (uriMatcher.match(uri)) {
+	      case QUAKES: 
+	        count = database.update(EarthquakeDatabaseHelper.EARTHQUAKE_TABLE, 
+	                                values, where, whereArgs);
+	        break;
+	      case QUAKE_ID: 
+	        String segment = uri.getPathSegments().get(1);
+	        count = database.update(EarthquakeDatabaseHelper.EARTHQUAKE_TABLE, 
+	                                values, KEY_ID
+	                                  + "=" + segment
+	                                  + (!TextUtils.isEmpty(where) ? " AND ("
+	                                  + where + ')' : ""), whereArgs);
+	        break;
+	      default: throw new IllegalArgumentException("Unknown URI " + uri);
+	    }
+
+	    getContext().getContentResolver().notifyChange(uri, null);
+	    return count;
+
+	}
+
+}
