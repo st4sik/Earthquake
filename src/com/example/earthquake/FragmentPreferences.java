1abd4de65da4001b31d1ee0ee465c78a283981ea
st4sik <ryabokonsm@mail.ru>
2014-06-07 14:43:03 +0400

    Добавил забатый класс FragmentPrefrences

---
+++ src/com/example/earthquake/FragmentPreferences.java
@@ -0,0 +1,11 @@
+package com.example.earthquake;
+import java.util.List;
+
+import android.preference.PreferenceActivity;
+public class FragmentPreferences extends PreferenceActivity{
+
+	  @Override
+	  public void onBuildHeaders(List<Header> target) {
+	    loadHeadersFromResource(R.xml.preference_headers, target);
+	  }
+	}
---
+++ src/com/example/earthquake/UserPreferenceFragment.java
@@ -0,0 +1,14 @@
+package com.example.earthquake;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public  class UserPreferenceFragment extends PreferenceFragment {
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.userpreferences);
+  }
+
+}
